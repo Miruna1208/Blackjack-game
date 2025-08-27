d1bb5c024e5f4b00df85d99a97fe9464782168cf
Miruna1208 <miruna.stanescu12@gmail.com>
2025-08-27 09:54:48 +0300

    [A, A] bug fixed

--- game.py
+++ game.py
@@ -12,9 +12,9 @@ def sum(hand):
             hand.remove(item)
             hand.append(item)
     for item in hand:
-        if item == "A" and s <= 10:
+        if item == "A" and s < 10:
             s += 11
-        elif item == "A" and s > 10:
+        elif item == "A" and s >= 10:
             s += 1
         elif item == "J":
             s += 12
@@ -49,8 +49,9 @@ answer = input()
 my_cards = []
 computer_cards = []
 if answer == 'y':
-    my_cards.append(random.choice(cards))
-    my_cards.append(random.choice(cards))
+  #  my_cards.append(random.choice(cards))
+  #  my_cards.append(random.choice(cards))
+    my_cards = ["A", "A"]
     computer_cards.append(random.choice(cards))
     computer_cards.append(random.choice(cards))
     for card in my_cards:
@@ -67,8 +68,8 @@ if answer == 'y':
                 print("Do you want to take one more card? (y/n)")
                 take = input()
                 if take == 'y':
-                    print(f"Your cards: {my_cards}")
                     take_card(my_cards, cards)
+                    print(f"Your cards: {my_cards}")
                 elif take == 'n':
                     ask = False
             if sum(computer_cards) < 17:
